a90175c4b94ce296790d978421f0278f13ee61b1
John Higgins <jhiggins29@qub.ac.uk>
2023-11-09 12:17:55 +0000

    fixed errors on previous commit

--- troubles-experience/src/pages/scales/scales.js
+++ troubles-experience/src/pages/scales/scales.js
@@ -7,13 +7,4 @@ const Scales = () =>{
   );
 }
 
-const NorthSouthCouncil = new Issue('North/South Ministerial Council',
-                                      'Establishment of blah blah blah',
-                                      5);
-
-
-const Decommissioning = new Issue('Weapon Decommissioning',
-                                      'Decommisioning of blah blah blah',
-                                      3);
-
 export default Scales;
