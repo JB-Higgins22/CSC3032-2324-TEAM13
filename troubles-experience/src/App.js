946589232ceb12085473ae720dd7610e20c16f46
John Higgins <jhiggins29@qub.ac.uk>
2023-11-09 11:35:16 +0000

    Paths set up for routing

--- troubles-experience/src/App.js
+++ troubles-experience/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './pages/home';
+import Scales from './pages/scales';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Whats the craic?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/scales' element={<Scales />} />
+      </Routes>
+    </Router>
       </header>
     </div>
   );
---
+++ troubles-experience/src/pages/scales.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Scales = () =>{
+  return (
+    <div>
+      <h3>Scales</h3>
+    </div>
+  );
+}
+export default Scales;
