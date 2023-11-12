cd7041c00a61c733667ddf36923177e823a199f1
John Higgins <jhiggins29@qub.ac.uk>
2023-11-12 11:02:44 +0000

    attempting to add issues to scales (BROKEN)

--- troubles-experience/src/classes/scales.js
+++ troubles-experience/src/classes/scales.js
@@ -1,6 +1,21 @@
-class ScalesObject{
-    constructor(unionistSide, nationalistSide) {
-        this.unionistSide = unionistSide;
-        this.nationalistSide = nationalistSide;
+export default class ScalesObject{
+    constructor(unionistIssues, nationalistIssues, unionistWeight, nationalistWeight) {
+        this.unionistIssues = unionistIssues;
+        this.nationalistIssues = nationalistIssues;
+        this.unionistWeight = unionistWeight;
+        this.nationalistWeight = nationalistWeight;
+    }
+
+    placeOnUnionist(issue) {
+        console.log(issue);
+        const currentIssue = [issue];
+        this.unionistIssues = this.unionistIssues.concat(currentIssue);
+        this.unionistWeight = this.unionistWeight + issue.weight;
+    }
+
+    placeOnNationalist(issue) {
+        const currentIssue = [issue];
+        this.nationalistIssues = this.nationalistIssues.concat(currentIssue);
+        this.nationalistWeight = this.nationalistWeight + issue.weight;
     }
 }
--- troubles-experience/src/pages/scales/scales.js
+++ troubles-experience/src/pages/scales/scales.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import ScalesObject from '../../classes/scales';
+import Issue from '../../classes/issue';
+
+var Issues = [];
+var peaceScales;
+
 const Scales = () =>{
+  initialiseIssues();
+  initialiseScales();
   return (
     <div>
-      <h3>Scales</h3>
+      <h1>Scales</h1>
+      <table>
+        <tr>
+          <th>Unionist Side</th>
+          <th>|</th>
+          <th>Nationalist Side</th>
+        </tr>
+          <td>{peaceScales.unionistWeight}</td>
+          <td>|</td>
+          <td>{peaceScales.nationalistWeight}</td>
+      </table>
+
+      <ul>
+        {Issues.map((issue) => (
+          <button key={issue.id} onClick={peaceScales.placeOnUnionist.bind(this, issue)}>{issue.name}</button>
+        ))}
+      </ul>
+
+      <ul>
+        {Issues.map((issue) => (
+          <button key={issue.id} onClick={peaceScales.placeOnNationalist.bind(this, issue)}>{issue.name}</button>
+        ))}
+      </ul>
+
     </div>
   );
+}
+
+function initialiseScales() {
+  peaceScales = new ScalesObject([{}],[{}],0,0);
+}
+
+function initialiseIssues() {
+  var decommissioningIssue = new Issue('Decommissioning', 'Decommissioning & blah blah blah', 10);
+
+  var northSouthCouncilIssue = new Issue('North/South Council', 'North/South Council & blah blah blah', 10);
+
+  var britishIrishCouncilIssue = new Issue('British/Irish Council', 'British/Irish Council & blah blah blah', 10);
+
+  var selfDeterminationIssue = new Issue('The Right to Self-Determination', 'The Right to Self-Determination & blah blah blah', 10);
+
+  Issues = [decommissioningIssue, northSouthCouncilIssue, britishIrishCouncilIssue, selfDeterminationIssue];
+
+  console.log(Issues);
 }
 
 export default Scales;
