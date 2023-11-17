ac40980e6fb94888528b54c3f771c33df6110d2d
John Higgins <jhiggins29@qub.ac.uk>
2023-11-17 13:05:24 +0000

    fixed basic version of scales engine

--- troubles-experience/src/classes/scales.js
+++ troubles-experience/src/classes/scales.js
@@ -1,21 +1,48 @@
-export default class ScalesObject{
+export default class ScalesObject {
     constructor(unionistIssues, nationalistIssues, unionistWeight, nationalistWeight) {
-        this.unionistIssues = unionistIssues;
-        this.nationalistIssues = nationalistIssues;
-        this.unionistWeight = unionistWeight;
-        this.nationalistWeight = nationalistWeight;
+      this.unionistIssues = unionistIssues;
+      this.nationalistIssues = nationalistIssues;
+      this.unionistWeight = unionistWeight;
+      this.nationalistWeight = nationalistWeight;
     }
-
+  
+    getUnionistIssues() {
+      return this.unionistIssues;
+    }
+  
     placeOnUnionist(issue) {
-        console.log(issue);
-        const currentIssue = [issue];
-        this.unionistIssues = this.unionistIssues.concat(currentIssue);
-        this.unionistWeight = this.unionistWeight + issue.weight;
+      const updatedUnionistIssues = [...this.unionistIssues, issue];
+      return new ScalesObject(updatedUnionistIssues, this.nationalistIssues, this.unionistWeight + issue.weight, this.nationalistWeight);
     }
-
+  
     placeOnNationalist(issue) {
-        const currentIssue = [issue];
-        this.nationalistIssues = this.nationalistIssues.concat(currentIssue);
-        this.nationalistWeight = this.nationalistWeight + issue.weight;
+      const updatedNationalistIssues = [...this.nationalistIssues, issue];
+      return new ScalesObject(this.unionistIssues, updatedNationalistIssues, this.unionistWeight, this.nationalistWeight + issue.weight);
     }
-}
+  }
+
+
+// export default class ScalesObject{
+//     constructor(unionistIssues, nationalistIssues, unionistWeight, nationalistWeight) {
+//         this.unionistIssues = unionistIssues;
+//         this.nationalistIssues = nationalistIssues;
+//         this.unionistWeight = unionistWeight;
+//         this.nationalistWeight = nationalistWeight;
+//     }
+
+//     getUnionistIssues() {
+//          return this.unionistIssues;
+//     }
+
+//     placeOnUnionist(issue) {
+//         const currentIssue = [issue];
+//         this.unionistIssues = this.unionistIssues.concat(currentIssue);
+//         this.unionistWeight = this.unionistWeight + issue.weight;
+//     }
+
+//     placeOnNationalist(issue) {
+//         const currentIssue = [issue];
+//         this.nationalistIssues = this.nationalistIssues.concat(currentIssue);
+//         this.nationalistWeight = this.nationalistWeight + issue.weight;
+//     }
+// }
--- troubles-experience/src/pages/scales/scales.js
+++ troubles-experience/src/pages/scales/scales.js
@@ -1,59 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ScalesObject from '../../classes/scales';
 import Issue from '../../classes/issue';
 
 var Issues = [];
-var peaceScales;
 
-const Scales = () =>{
-  initialiseIssues();
-  initialiseScales();
+const Scales = () => {
+  const [peaceScales, setPeaceScales] = useState(null);
+
+  useEffect(() => {
+    initialiseScales();
+    initialiseIssues();
+  }, []); // The empty dependency array ensures the effect runs only once on mount
+
+  function initialiseScales() {
+    const initialisationIssue = new Issue('Decommissioning', 'Decommissioning & blah blah blah', 10);
+    setPeaceScales(new ScalesObject([], [], 0, 0));
+  }
+
+  function initialiseIssues() {
+    const decommissioningIssue = new Issue('Decommissioning', 'Decommissioning & blah blah blah', 10);
+    const northSouthCouncilIssue = new Issue('North/South Council', 'North/South Council & blah blah blah', 10);
+    const britishIrishCouncilIssue = new Issue('British/Irish Council', 'British/Irish Council & blah blah blah', 10);
+    const selfDeterminationIssue = new Issue(
+      'The Right to Self-Determination',
+      'The Right to Self-Determination & blah blah blah',
+      10
+    );
+
+    Issues = [decommissioningIssue, northSouthCouncilIssue, britishIrishCouncilIssue, selfDeterminationIssue];
+  }
+
   return (
     <div>
       <h1>Scales</h1>
       <table>
-        <tr>
-          <th>Unionist Side</th>
-          <th>|</th>
-          <th>Nationalist Side</th>
-        </tr>
-          <td>{peaceScales.unionistWeight}</td>
-          <td>|</td>
-          <td>{peaceScales.nationalistWeight}</td>
+        <tbody>
+          <tr>
+            <th>Unionist Side</th>
+            <th>|</th>
+            <th>Nationalist Side</th>
+          </tr>
+          <tr>
+            <td>{peaceScales?.unionistWeight}</td>
+            <td>|</td>
+            <td>{peaceScales?.nationalistWeight}</td>
+          </tr>
+        </tbody>
       </table>
 
       <ul>
         {Issues.map((issue) => (
-          <button key={issue.id} onClick={peaceScales.placeOnUnionist.bind(this, issue)}>{issue.name}</button>
+          <button key={issue.id} onClick={() => setPeaceScales(prevScales => prevScales.placeOnUnionist(issue))}>
+            {issue.name}
+          </button>
         ))}
       </ul>
 
       <ul>
         {Issues.map((issue) => (
-          <button key={issue.id} onClick={peaceScales.placeOnNationalist.bind(this, issue)}>{issue.name}</button>
+          <button key={issue.id} onClick={() => setPeaceScales(prevScales => prevScales.placeOnNationalist(issue))}>
+            {issue.name}
+          </button>
         ))}
       </ul>
-
     </div>
   );
-}
-
-function initialiseScales() {
-  peaceScales = new ScalesObject([{}],[{}],0,0);
-}
-
-function initialiseIssues() {
-  var decommissioningIssue = new Issue('Decommissioning', 'Decommissioning & blah blah blah', 10);
-
-  var northSouthCouncilIssue = new Issue('North/South Council', 'North/South Council & blah blah blah', 10);
-
-  var britishIrishCouncilIssue = new Issue('British/Irish Council', 'British/Irish Council & blah blah blah', 10);
-
-  var selfDeterminationIssue = new Issue('The Right to Self-Determination', 'The Right to Self-Determination & blah blah blah', 10);
-
-  Issues = [decommissioningIssue, northSouthCouncilIssue, britishIrishCouncilIssue, selfDeterminationIssue];
-
-  console.log(Issues);
-}
+};
 
 export default Scales;
