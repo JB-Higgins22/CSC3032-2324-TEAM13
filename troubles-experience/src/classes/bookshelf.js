6f29caf5f6c70cf0b2758b2b493212adeb97028b
John Higgins <jhiggins29@qub.ac.uk>
2024-01-26 12:19:16 +0000

    Redesigned interface, animated scales

--- troubles-experience/src/classes/bookshelf.js
+++ troubles-experience/src/classes/bookshelf.js
@@ -7,5 +7,22 @@ export default class BookshelfObject {
       return this.issues;
     }
 
+    removeBook(issue) {
+        if (this.checkIfOnBookshelf(issue)) {
+            var index = this.issues.indexOf(issue);
+            this.issues.splice(index, 1);
+        }
+        
+        return new BookshelfObject(this.issues);
+    }
+
+    checkIfOnBookshelf(issue) {
+        if (this.issues.indexOf(issue) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
   }
 
